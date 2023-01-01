8e3cf6b4c293169d88fcbfb83f298705aa561fe8
开心 莫 <2944359631@qq.com>
2023-01-01 17:02:45 +0800

    创建后端

--- backend/src/main/java/com/kob/backend/controller/pk/BotlnfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotlnfoController.java
@@ -1,2 +1,22 @@
-package com.kob.backend.controller.pk;public class BotlnfoController {
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/pk/")
+public class BotlnfoController {
+
+    @RequestMapping("getbotinfo/")
+    public Map<String, String> getBotInfo() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "tiger");
+        map.put("ratind", "1500");
+        return map;
+    }
 }
