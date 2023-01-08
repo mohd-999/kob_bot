52a768eb9481c5701aa0ba2f42bac2b0e24652cb
开心 莫 <2944359631@qq.com>
2023-01-09 04:06:47 +0800

    配置Mysql和测试后端和数据库连通性

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -1,15 +1,14 @@
-package com.example.demo.controller.pk;
+package com.kob.backend.controller.pk;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
-public class BotlnfoController {
+public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
     public Map<String, String> getBotInfo() {
